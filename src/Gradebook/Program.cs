84832d97531777a0f3c94834ac3dd79cb3a0de64
Greg Ingersoll <gpipuck@gmail.com>
2020-07-11 17:08:10 -0400

    update

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -7,6 +8,8 @@ namespace Gradebook
       static void Main(string[] args)
       {
          var numbers = new [] { 12.7, 10.3, 0.11, 4.1 };
+         
+         List<double> grades;
 
          var result = 0.0;
          foreach (var number in numbers)
