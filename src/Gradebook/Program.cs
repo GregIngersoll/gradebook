81759afea64dd7106f75a154974e202d69f79485
Greg Ingersoll <gpipuck@gmail.com>
2020-07-12 17:01:53 -0400

    update

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -6,18 +6,19 @@ namespace Gradebook
    class Program
    {
       static void Main(string[] args)
-      {
-         var numbers = new [] { 12.7, 10.3, 0.11, 4.1 };
-         
-         List<double> grades;
+      {         
+         var grades = new List <double>() { 12.7, 10.3, 0.11, 4.1 };
+         grades.Add(56.1);
 
          var result = 0.0;
-         foreach (var number in numbers)
+         foreach (var number in grades)
          {
             result += number;
          }
 
-         System.Console.WriteLine(result);
+         var mean = result / grades.Count;
+
+         System.Console.WriteLine($"The result is {result:N1} and the average (mean) is {mean:N1}");
 
          if (args.Length > 0)
          {
